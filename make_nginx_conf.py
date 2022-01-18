374b162285381544f7cc0fc3f084dbcde3b52430
starplatinum111 <5428527+starplatinum111@user.noreply.gitee.com>
2022-01-18 22:32:14 +0800

    nginx conf dir 是在port 后面

--- make_nginx_conf.py
+++ make_nginx_conf.py
@@ -14,15 +14,21 @@ nginx_template:str
 out_str=nginx_template.replace("$port$", port)
 out_str=out_str.replace("$root_path$", root_path)
 
+
+
+
+nginx_root_dir=f"/home/nginx/{port}"
+
 # out_path=f"/home/nginx/nginx_{port}.conf"
-out_path=f"nginx_{port}.conf"
+out_path=f"{nginx_root_dir}/nginx_{port}.conf"
+# out_path=f"nginx_{port}.conf"
 with open(out_path,"w",encoding="utf-8") as f:
     # nginx_template= f.read()
     f.write(out_str)
 
 print("config write here",out_path)
 
-nginx_root_dir=f"/home/nginx/{port}"
+
 os.system(f"mkdir {nginx_root_dir}")
 os.system(f"cp /usr/sbin/nginx  {nginx_root_dir}/nginx")
 
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 使用说明: 使用backup_proj_src 修改当前路径和 路径即可使用
+
+文档
+https://acc15t4bm5.feishu.cn/docs/doccnLdlkjx4L1SmPcvDHNESMic
